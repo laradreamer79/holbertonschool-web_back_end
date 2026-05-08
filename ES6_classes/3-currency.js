de15a3c9cdf03da7babad997bf987ed543db1a2d
laradreamer79 <laradreamer79@gmail.com>
2026-05-08 14:14:24 +0300

    currency

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,7 +1,23 @@
-class Currency{
+export default class Currency {
   constructor(code, name) {
-    this._code = String(code);
-    this._name = String(name);
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
   }
 
   displayFullCurrency() {
