ee585c55777db53493461b3136e02571aa3e5bfe
laradreamer79 <laradreamer79@gmail.com>
2026-05-08 14:47:51 +0300

    cars

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,28 +1,16 @@
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-}
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    const Constructor = this.constructor[Symbol.species];
+    return new Constructor();
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 }
-
-const car1 = new Car('Tesla', 'electric', 'red');
-const car2 = car1.cloneCar();
-
-export { Car, car1, car2 };
